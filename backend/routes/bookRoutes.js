d2b3d87f5608def688ebe348e44269caefd1ab1d
Mesfin67 <140214673+Mesfin67@users.noreply.github.com>
2025-04-09 14:54:25 +0300

    update backend

--- backend/routes/bookRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getBooks, createBook, deleteBook } = require('../controllers/bookController');
-const { protect } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.route('/').get(protect, getBooks).post(protect, createBook);
-router.route('/:id').delete(protect, deleteBook);
-
-module.exports = router;
